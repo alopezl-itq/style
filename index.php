d0c9c8c16e671cb27f9463a32a642506ce5fd9db
EddTrejo <edd.get.a.break@hotmail.com>
2016-03-29 17:16:31 -0600

    Login
    Carpetas de Login

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
 <?php
-echo "string"; //probando
-echo date();
+include('includes/comunes.php');
+//require('includes/class.Conexion.php');
+//$db = new Conexion();
 
-echo "string"; //Nestor
-echo "string"; //Bryan
+$modo = isset($_GET['modo']) ? $_GET['modo'] : 'default';
+
+switch($modo){
+	case 'login':
+			if(isset($_POST['login'])){
+				if(!empty($_POST['user']) and !empty($_POST['pass'])){
+					
+			}else{
+				header('location: index.html');
+			}		
+			}else{
+				header('location: index.html');
+			}
+	break;
+	case 'registro':
+		echo'registro';
+	break;
+	default:
+	//$template = new Prinick();
+	//$template -> display('index.html');
+	break;
+	
+	}
 
 ?>
 
